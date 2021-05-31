b1898de69ce545746f58dae67332f8b81dedf066
Anton Sidko <sidko.antonanatoljevich@gmail.com>
2021-05-31 11:01:56 +0300

    first commit

---
+++ tabs on native JS/tabs.js
@@ -0,0 +1,38 @@
+// Перевод в строгий режим
+'use stirct';
+
+window.addEventListener('DOMContentLoaded', function() {
+
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+    function hidetabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+
+    hidetabContent(1);
+
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+
+    info.addEventListener('click', function(event) {
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for(let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hidetabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
+});
