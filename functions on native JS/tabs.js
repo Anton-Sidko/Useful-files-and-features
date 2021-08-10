83575b8d541423f0aaa651c49c225a6e01f3e2f2
Anton Sidko <81675066+Anton-Sidko@users.noreply.github.com>
2021-08-10 17:02:18 +0300

    Update tabs.js

--- functions on native JS/tabs.js
+++ functions on native JS/tabs.js
@@ -61,3 +61,55 @@ window.addEventListener('DOMContentLoaded', function() {
 
     showTabModal ();
 });
+
+
+//Лучший модульный способ
+// Пишем модуль функций, которые потом экспортируем в основной файл скрипта
+const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
+    const header = document.querySelector(headerSelector),
+            tab = document.querySelectorAll(tabSelector),
+            content = document.querySelectorAll(contentSelector);
+
+    //Функция которая скрывает весь контент и убирает класс активности со всех табов
+    function hideTabContent() {
+        content.forEach(item => {
+            //Лучше классами
+            item.style.display = 'none';
+        });
+
+        tab.forEach(item => {
+            item.classList.remove(activeClass);
+        });
+    }
+
+    //Функция которая показывает i-й контент и ставит класс на i-й таб. Прописан i = 0 по умолчанию, чтобы при первом вызове не передавать аргумент
+    function showTabContent(i = 0) {
+        content[i].style.display = 'block';
+        tab[i].classList.add(activeClass);
+    }
+
+    //Первоначальный вызов функций при загрузке страницы. showTabContent() без аргумента, так как i = 0 по умолчанию
+    hideTabContent();
+    showTabContent();
+
+    //Назначаем обработчик событий на обертку табов
+    header.addEventListener('click', (e) => {
+        const target = e.target;
+        //Проверка, что таргет есть
+        if (target &&
+            (target.classList.contains(tabSelector.replace(/\./, "")) || //Проверка, что у таргета нужный класс. tabSelector.replace(/\./, "") так как classList.contains нужен класс без точки, а у селектора точка
+            target.parentNode.classList.contains(tabSelector.replace(/\./, "")))) {  //Проверка, что у родителя таргета нужный класс (если кликнули на элемент-ребенок таба). tabSelector.replace(/\./, "") так как classList.contains нужен класс без точки, а у селектора точка
+            //Перебор псевдомассива всех табов и вызов функций только для таба с индексом целью
+            tab.forEach((item, i) => {
+                if (target == item || target.parentNode == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+
+};
+
+export default tabs;
+//END
