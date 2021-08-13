78038164a240b8e679a4fc2e0f003721d85a5cfd
Anton Sidko <81675066+Anton-Sidko@users.noreply.github.com>
2021-08-13 17:00:22 +0300

    Update timer.js

--- functions on native JS/timer.js
+++ functions on native JS/timer.js
@@ -54,3 +54,75 @@ function setClock(id, endtime) {
 }
 
 setClock('timer', deadline);
+
+
+
+//Модуль таймера
+const timer = (id, deadline) => {
+
+    //Добавляем 0 для чисел меньше 10, для правильногоотобрадения (03, 02...)
+    const addZero = (num) => {
+        if (num <= 9) {
+            return '0' + num;
+        } else{
+            return num;
+        }
+    };
+
+    //Создаем обьект в котором хранится разница между дедлайном и текущим моментом Date.parse(new Date())
+    const getTimeRemaining = (endtime) => {
+        const t       = Date.parse(endtime) - Date.parse(new Date()),
+              seconds = Math.floor((t / 1000) % 60),
+              minutes = Math.floor((t /(1000 * 60)) % 60),
+              hours   = Math.floor((t /(1000 * 60 * 60)) % 24),
+              days    = Math.floor((t /(1000 * 60 * 60 * 24)));
+
+        return {
+            'total'   : t,
+            'days'    : days,
+            'hours'   : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+    };
+
+    //Запускаем таймер
+    const setClock = (selector, endtime) => {
+        const timer        = document.querySelector(selector),
+              days         = timer.querySelector('#days'),
+              hours        = timer.querySelector('#hours'),
+              minutes      = timer.querySelector('#minutes'),
+              seconds      = timer.querySelector('#seconds'),
+              //Интервал каждую секунду
+              timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            const time = getTimeRemaining(endtime);
+
+            //Отображаем таймер на странице
+            days.textContent = addZero(time.days);
+            hours.textContent = addZero(time.hours);
+            minutes.textContent = addZero(time.minutes);
+            seconds.textContent = addZero(time.seconds);
+
+            //Отображаем 00 если дедлайн прошел
+            if (time.total <= 0) {
+                days.textContent = '00';
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+
+                clearInterval(timeInterval);
+            }
+        }
+
+        //Запускаем вручную, так как через интервал вызоветься только через 1 секунуд и при обновлении страницы это будет заметно
+        updateClock();
+    };
+
+    //Вызываем функцию запуска таймера
+    setClock(id, deadline);
+};
+
+export default timer;
+//END
