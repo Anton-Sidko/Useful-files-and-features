8e64fe8efe96fc60857ebd4615accd65f4a3fdd0
Anton Sidko <81675066+Anton-Sidko@users.noreply.github.com>
2021-08-18 13:02:40 +0300

    Update slider.js

--- functions on native JS/slider.js
+++ functions on native JS/slider.js
@@ -60,3 +60,88 @@ for (let i = 0; i < dots.length + 1; i++) {
     }
 }
 });
+
+
+
+//Модуль слайдера
+const sliders = (slides, direction, prev, next) => {
+	let slideIndex = 1, //Отображает текущий слайд
+		paused = false; //Для паузы автопереключения при hover
+
+	const items = document.querySelectorAll(slides);
+
+
+	function showSlides(n) {
+		if (n > items.length) {
+			slideIndex = 1;
+		}
+
+		if (n < 1) {
+			slideIndex = items.length;
+		}
+
+		items.forEach(item => {
+			item.classList.add('animated');
+			item.style.display = 'none';
+		});
+
+		items[slideIndex - 1].style.display = 'block';
+	}
+
+	showSlides(slideIndex);
+
+	function changeSlides(n) {
+		showSlides(slideIndex += n);
+	}
+
+	// Кнопки не всегда есть на странице, поэтому нужен обработчик ошибок
+	try {
+		const prevBtn = document.querySelector(prev),
+			  nextBtn = document.querySelector(next);
+
+		prevBtn.addEventListener('click', () => {
+			changeSlides(-1);
+			items[slideIndex - 1].classList.remove('slideInLeft');
+			items[slideIndex - 1].classList.add('slideInRight');
+		});
+
+		nextBtn.addEventListener('click', () => {
+			changeSlides(1);
+			items[slideIndex - 1].classList.remove('slideInRight');
+			items[slideIndex - 1].classList.add('slideInLeft');
+		});
+	} catch(e) {}
+
+
+	// Пауза авто переключения
+	function activateAnimation() {
+		//Направление переключения слайдов
+		if (direction === 'vertical') {
+			paused = setInterval(function() {
+				changeSlides(1);
+				items[slideIndex - 1].classList.add('slideInDown');
+			}, 3000);
+		} else {
+			paused = setInterval(function() {
+				changeSlides(1);
+				items[slideIndex - 1].classList.remove('slideInRight');
+				items[slideIndex - 1].classList.add('slideInLeft');
+			}, 5000);
+		}
+	}
+
+	//Навешиваем обработчик событий на родителя слайда, чтобы ставить авто переключение на паузу
+	items[0].parentNode.addEventListener('mouseenter', () => {
+		clearInterval(paused);
+	});
+	items[0].parentNode.addEventListener('mouseleave', () => {
+		activateAnimation();
+	});
+
+	activateAnimation();
+
+};
+
+export default sliders;
+
+//END
